ec226708d062cb4fe3f1720b2ca9322bde0dc82e
binibenny95 <binibenny95@gmail.com>
2024-07-08 11:16:37 +0200

    madlis

---
+++ code/hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3.10
+noun = input("noun:")
+verb = input("verb:")
+
+sentence = f"i love to see {noun} fly high .I love you so much my kid.you are \ my everything , i know you love to {verb}.Keep flying my sunshine."
+print(sentence)
